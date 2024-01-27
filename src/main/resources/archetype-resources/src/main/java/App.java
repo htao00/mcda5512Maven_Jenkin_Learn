2adb5213b12f0cafb1138681e59f63cea7e84aeb
htao00 <taohaodong00@gmail.com>
2024-01-26 20:20:51 -0400

    first commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.smu.mscda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
